50d5139103bf336fd18189d5b963f14ff816b607
yRaposo <ynyght@gmail.com>
2023-10-31 17:45:01 -0300

    aulas iniciais | console, variaveis e tipo de dado

---
+++ Aula 4 - Variaveis/Assets/js/script.js
@@ -0,0 +1,5 @@
+let x = 10;
+let y = 20;
+let z = "uga";
+
+console.log(z + (x + y));
---
+++ Aula 5 - Tipo de Dados/Assets/js/script.js
@@ -0,0 +1,9 @@
+let x = 10;
+let y = 20.91;
+let z = "uga";
+let logado = false;
+let lista = ["Farinha","Ovo","açucar","Limão","Fermento"];
+let Objeto = {Formato: "redondo", Material: "Elastico", Tamanho: 30}
+
+console.log(z + (x + y + Objeto.Tamanho) + Objeto.Material);
+console.log(typeof z)
