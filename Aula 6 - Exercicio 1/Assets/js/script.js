a870b188c0d1792431af2cf8e3ec0d388dea60e1
yRaposo <ynyght@gmail.com>
2023-10-31 17:56:35 -0300

    Aula 6 | Exercicio 1

---
+++ Aula 6 - Exercicio 1/Assets/js/script.js
@@ -0,0 +1,18 @@
+// Exercicio 1
+
+let carro = "Ferrari";
+
+console.log(carro);
+
+// Exercicio 2
+
+let Bolo = { Moeda: "R$", Valor: "19.99" };
+
+console.log(Bolo.Moeda + Bolo.Valor);
+
+// Exercicio 3: Corrija o código a seguir (e exiba-o).
+// let "cidade" = São Paulo
+
+let cidade = "São Paulo";
+
+console.log(cidade);
