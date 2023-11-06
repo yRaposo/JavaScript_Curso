835bd710f1914c45d29e08695df6f5eb9f28a19b
yRaposo <ynyght@gmail.com>
2023-11-06 13:54:50 -0300

    aulas 7 a 12

---
+++ Aula 10 - let const var/Assets/js/script.js
@@ -0,0 +1,17 @@
+let a = "string";
+console.log(a);
+
+a = "integer";
+console.log(a);
+
+var b = "boolean";
+console.log(b);
+
+b = "Double";
+console.log(b);
+
+const c = "bit";
+console.log(c);
+
+c = "float";
+console.log(c);
---
+++ Aula 12 - String e Template string/Assets/js/script.js
@@ -0,0 +1,10 @@
+let nome = "Fulano";
+let sobrenome = "Sicrano";
+let idade = 200;
+
+// let nomeCompleto = nome + ' ' + sobrenome;
+let nomeCompleto = `Seu nome é ${nome} ${sobrenome}?`
+
+let idadeString = `você tem ${idade} anos?`
+
+console.log(nomeCompleto, idadeString);
